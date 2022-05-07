0b4af0e0c2fc6867c7f6608749658354ea909eef
Glitch (glitch-hello-react) <none>
2022-05-07 05:39:03 +0000

    📒🌯 Checkpoint ./index.html:25677769/1141 ./src/index.jsx:25677769/747 ./src/app.jsx:25677769/1886

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-/**
-* Root of react site 
-*
-* Imports Helment provider for the page head
-* And App which defines the content and navigation
-*/
-
-// Render the site https://reactjs.org/docs/react-dom.html#render
-ReactDOM.render(
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
