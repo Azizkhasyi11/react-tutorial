afb176642b9520178dbf0781a0c538ab6914d4c0
Glitch (glitch-hello-react) <none>
2022-05-09 05:26:27 +0000

    🏯🕴 Checkpoint ./src/components/Trending.jsx:25677769/4256 ./src/style/LandingPage.css:25677769/294

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,18 +1,27 @@
+import { Card, Container, Col, Row, Image } from "react-bootstrap";
+
 const Trending = () => {
-  return(
+  return (
     <div>
-      <Card>
-        <Card.Img />
-        <Card.ImgOverlay>
-          <Card.Title>Title</Card.Title>
-          <Card.Text>
-          BRUH
-          </Card.Text>
-        </Card.ImgOverlay>
-        <Card.Text>Ok</Card.Text>
-      </Card>
+      <Container>
+        <Row>
+          <Col className="movieImage">
+            <Card className="text-dark text-center movieImage">
+              <Image
+                src="https://github.com/deaaprizal/rdeafilms-react-js-basic/blob/part1/basic/src/assets/images/trending/dune.jpg?raw=true"
+                alt="Dune Image"
+                width="50%"
+                height="auto"
+              />
+                <Card.Title>Title</Card.Title>
+                <Card.Text>BRUH</Card.Text>
+                <Card.Text>Ok</Card.Text>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
 
-export default Trending; 
+export default Trending;
